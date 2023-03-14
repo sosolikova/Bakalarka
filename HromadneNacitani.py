7c427fc24844782bb90a149c92b37885b7e8b21f
Veronika Sosolikova <v_sosolikova@utb.cz>
2023-03-14 16:12:15 +0100

    Function for loading files from directory

---
+++ HromadneNacitani.py
@@ -0,0 +1,48 @@
+import os
+import pandas as pd
+import Funkce
+
+dtypes_odpady = {
+    'Evident':              'string',
+    'Evident_A':            'string',
+    'Evident_TypSubjektu':  'string',
+    'Evident_Nazev_A':      'string',
+    'Evident_Kraj':         'string',
+    'Partner':              'string',
+    'Partner_A':            'string',
+    'Partner_TypSubjektu':  'string',
+    'Partner_Nazev':        'string',
+    'Partner_Nazev_A':      'string',
+    'Partner_Kraj':         'string',
+    'Kod':                  'string',
+    'Indikator':            'string',
+    'Mnozstvi':             'float'
+}
+
+'Funkce pro načtení dat z CSV do DataFrame'
+def load_csv_type_conversion(filename, dtypes):
+    df = pd.read_csv(filename, delimiter=';', decimal=',')
+
+    for column, dtype in dtypes.items():
+        if column in df.columns:
+          df[column].astype(dtype)
+    return df
+
+def load_files_to_df(directory,extension):
+    files = [file for file in os.listdir(directory) if file.endswith(extension)]
+
+    df = pd.DataFrame()
+
+    for file in files:
+        filepath = os.path.join(directory, file)
+        data = pd.read_csv(filepath,delimiter=';', decimal=',', usecols = None)
+        filename = os.path.splitext(file)[0]
+        data['source_file'] = filename
+        df = df.append(data)
+        print('________---sloucene soubory ----__________')
+        print(df)
+    return df
+
+Zdrojovy = load_files_to_df('Data','.csv')
+
+Funkce.save_dataframe_to_csv(Zdrojovy,'Zdrojovy')
