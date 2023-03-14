81a6848c306cebba8365464b79539ae90ab856c2
Veronika Sosolikova <v_sosolikova@utb.cz>
2023-03-14 16:38:57 +0100

    Change dtype from dtypes structure

--- HromadneNacitani.py
+++ HromadneNacitani.py
@@ -2,10 +2,11 @@ import os
 import pandas as pd
 import Funkce
 
-dtypes_odpady = {
+dtypes_odpady2= {
     'Evident':              'string',
     'Evident_A':            'string',
     'Evident_TypSubjektu':  'string',
+    'Evident_Nazev':        'string',
     'Evident_Nazev_A':      'string',
     'Evident_Kraj':         'string',
     'Partner':              'string',
@@ -28,20 +29,23 @@ def load_csv_type_conversion(filename, dtypes):
           df[column].astype(dtype)
     return df
 
-def load_files_to_df(directory,extension):
+def load_files_to_df(directory,extension,dtypes):
     files = [file for file in os.listdir(directory) if file.endswith(extension)]
 
     df = pd.DataFrame()
 
     for file in files:
         filepath = os.path.join(directory, file)
-        data = pd.read_csv(filepath,delimiter=';', decimal=',', usecols = None)
+        data = pd.read_csv(filepath,delimiter=';', decimal=',', usecols = None, dtype=dtypes)
+
         filename = os.path.splitext(file)[0]
-        data['source_file'] = filename
+        data['Zdrojovy_soubor'] = filename
         df = df.append(data)
     return df
 
-Zdrojovy = load_files_to_df('Data','.csv')
+Zdrojovy2 = load_files_to_df('Data','.csv',dtypes_odpady2)
 print('________---sloucene soubory ----__________')
-print(Zdrojovy)
-Funkce.save_dataframe_to_csv(Zdrojovy,'Zdrojovy')
+print(Zdrojovy2)
+Funkce.save_dataframe_to_csv(Zdrojovy2,'Zdrojovy2')
+
+Zdrojovy2.info()
