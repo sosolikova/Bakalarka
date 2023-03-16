5af378b983f8351e3371eccf236c6c991646286f
Veronika Sosolikova <v_sosolikova@utb.cz>
2023-03-17 00:21:26 +0100

    GUI_dropDownBox

---
+++ GUI_dropdownBox.py
@@ -0,0 +1,46 @@
+from tkinter import *
+from tkinter import ttk
+from PIL import ImageTk, Image
+from pandas import options
+
+
+root = Tk()
+root.title('Data o odpadech')
+'root.iconbitmap(c:/gui/codemy.ico)'
+root.geometry("400x400")
+
+def selected(event):
+    if clicked.get() == 'Friday':
+        myLabel = Label(root, text="Yay Its Friday").pack()
+    else:
+        myLabel = Label(root, text=clicked.get()).pack()
+
+def comboclick(event):
+    #myLabel = Label(root, text=myCombo.get()).pack()
+    if myCombo.get() == 'Friday':
+        myLabel = Label(root, text="Yay Its Friday").pack()
+    else:
+        myLabel = Label(root, text=myCombo.get()).pack()
+            
+options = [
+    "Monday",
+    "Tueseday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+]
+clicked = StringVar()
+clicked.set(options[0])
+
+drop = OptionMenu(root, clicked, *options, command=selected)
+drop.pack(pady=20)
+
+myCombo = ttk.Combobox(root, value=options)
+myCombo.current(0)
+myCombo.bind("<<ComboboxSelected>>", comboclick)
+myCombo.pack()
+#myButton = Button(root, text="select from list",command=selected)
+#myButton.pack()
+root.mainloop()
