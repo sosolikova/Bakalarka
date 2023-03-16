acaab040e746e27697d2ddcaa06122c40bc243de
Veronika Sosolikova <v_sosolikova@utb.cz>
2023-03-16 22:15:46 +0100

    GUI kick off

---
+++ GUI.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from PIL import Image
+
+root = Tk()
+root.title('Data o odpadech')
+'root.iconbitmap(c:/gui/codemy.ico)'
+root.geometry("400x200")
+
+
+root.mainloop()
--- HromadneNacitani.py
+++ HromadneNacitani.py
@@ -167,3 +167,32 @@ Produkce_and_Prevzeti = Zdrojovy_kody_mnozstvi[(Zdrojovy_kody_mnozstvi['Indikato
 print(Produkce_and_Prevzeti.groupby('Indikator')['ZmenaMnozstvi'].agg([np.mean,np.median])) 
 
 
+import tkinter as tk
+
+def handle_selection(selection):
+    """Funkce, která bude spuštěna při výběru položky v menu"""
+    print(f"Vybráno: {selection}")
+
+# Vytvoření okna
+root = tk.Tk()
+
+# Vytvoření menu
+menu = tk.Menu(root)
+root.config(menu=menu)
+
+# Vytvoření položek v menu
+file_menu = tk.Menu(menu)
+menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="New", command=lambda: handle_selection("New"))
+file_menu.add_command(label="Open", command=lambda: handle_selection("Open"))
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
+
+edit_menu = tk.Menu(menu)
+menu.add_cascade(label="Edit", menu=edit_menu)
+edit_menu.add_command(label="Cut", command=lambda: handle_selection("Cut"))
+edit_menu.add_command(label="Copy", command=lambda: handle_selection("Copy"))
+edit_menu.add_command(label="Paste", command=lambda: handle_selection("Paste"))
+
+# Spuštění hlavní smyčky
+root.mainloop()
