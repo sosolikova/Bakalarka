ef3d658289b3097fca46b303a85a8b4b0c1ed2dd
Veronika Sosolikova <v_sosolikova@utb.cz>
2023-03-19 10:51:39 +0100

    Set legend to gdf_kraje

--- NacteniJSON.py
+++ NacteniJSON.py
@@ -4,11 +4,23 @@ import matplotlib.pyplot as plt
 
 # Načtení souboru
 
-gdf = gpd.read_file('kraje-simple.json', encoding='utf-8')
+gdf_kraje = gpd.read_file('kraje-simple.json', encoding='utf-8')
+gdf_orp = gpd.read_file('orp-simple.json', encoding='utf-8')
+gdf_obce = gpd.read_file('obce-simple.json', encoding='utf-8')
 
 # Vypsání načtených dat
 print('_________nactene data__________-')
-print(gdf)
-gdf.plot()
+#print(gdf)
+leg_kwds={'title': 'NAZEV', 
+          'loc': 'upper left',
+          'bbox_to_anchor': (1,1.03),
+          'ncol':1}
+gdf_kraje.plot(column = 'NAZEV',
+               cmap = 'Accent',
+               legend = True,
+               legend_kwds = leg_kwds)
+plt.title('Odpad dle krajů')
+#gdf_orp.plot(column = 'NAZEV',legend = False)
+#gdf_obce.plot(column = 'NAZEV',legend = False)
 plt.show()
 
