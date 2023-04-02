f3c904e78ffa8f8ce26b54a337069764a2fd6754
Veronika Sosolikova <v_sosolikova@utb.cz>
2023-04-02 23:35:07 +0200

    Add data 'ZmenaMnozstvi'

--- NacteniJSON.py
+++ NacteniJSON.py
@@ -1,6 +1,15 @@
 import json
 import geopandas as gpd
 import matplotlib.pyplot as plt
+import HromadneNacitani as hn
+
+# ziskani df s vyfiltrovanými údaji pro Indikator 'Produkce' a použita funkce sum za jednotlivé kraje
+indikator_map_kraje = hn.Zdrojovy_kody_mnozstvi[(hn.Zdrojovy_kody_mnozstvi['Indikator'] == 'Produkce') & (hn.Zdrojovy_kody_mnozstvi['Druh_Odpadu'] == '200111')]
+kraje_produkce = hn.group_data_by_columns(indikator_map_kraje,'ZmenaMnozstvi','Evident_Kraj_Nazev','Indikator')
+print('___----indikator-map - kraje_produkce ___________')
+print(kraje_produkce.head)
+
+
 
 # Načtení souboru
 
@@ -8,14 +17,19 @@ gdf_kraje = gpd.read_file('kraje-simple.json', encoding='utf-8')
 gdf_orp = gpd.read_file('orp-simple.json', encoding='utf-8')
 gdf_obce = gpd.read_file('obce-simple.json', encoding='utf-8')
 
+
+#Sloučení df kraje_produkce s geometrickým df podle názvu kraje
+gdf_merged = gdf_kraje.merge(kraje_produkce, left_on='NAZEV', right_on='Evident_Kraj_Nazev')
+
 # Vypsání načtených dat
 print('_________nactene data__________-')
-#print(gdf)
-leg_kwds={'title': 'NAZEV', 
+print(gdf_merged)
+leg_kwds={'title': 'ZmenaMnozstvi', 
           'loc': 'upper left',
           'bbox_to_anchor': (1,1.03),
           'ncol':1}
-gdf_kraje.plot(column = 'NAZEV',
+# Použití metody plot() pro zobrazení mapy s barvami krajů podle hodnot ze sloupce 'ZmenaMnozstvi' v novém datovém rámci gdf_merged.
+gdf_merged.plot(column = 'NAZEV',
                cmap = 'Accent',
                legend = True,
                legend_kwds = leg_kwds)
